430d99ef5a051c1c4252ddc55a4477607b1f1c93
Sekou <sekouk1410@gmail.com>
2025-10-26 22:08:56 +0000

    Ajout de la page mes projets

--- lib/features/novice/screens/projects_page.dart
+++ lib/features/novice/screens/projects_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-
 class NoviceProjectsPage extends StatelessWidget {
   const NoviceProjectsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text('Projet (Novice)')),
+      body: Center(child: Text('Projet (Novice)')), // placeholder
     );
   }
 }
